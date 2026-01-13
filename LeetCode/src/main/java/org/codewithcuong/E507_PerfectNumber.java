7923ed9979f055ad4968fa11bc55e213747aa6c4
Đào Chí Cường <cuong16032005@gmail.com>
2026-01-13 15:15:48 +0700

    E507

--- LeetCode/src/main/java/org/codewithcuong/E507_PerfectNumber.java
+++ LeetCode/src/main/java/org/codewithcuong/E507_PerfectNumber.java
@@ -2,7 +2,13 @@ package org.codewithcuong;
 
 public class E507_PerfectNumber {
     public boolean checkPerfectNumber(int num) {
-        
+        int check = 0;
+        for(int i = 1; i <= num/2; i++){
+            if(num % i ==0){
+                check += i;
+            }
+        }
+        if(check == num) return true;
         return false;
     }
 }
